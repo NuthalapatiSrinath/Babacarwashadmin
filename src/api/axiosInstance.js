691c6b94a0bab493d42ba5984adf6fe44cf74fd6
Srinath Nuthalapati <srynath13@gmail.com>
2026-01-14 23:14:14 +0530

    don

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import toast from "react-hot-toast";
 
 // HARDCODED BASE URL
-// This forces the request to be relative (e.g., https://your-site.com/api/...)
+// This forces the request to be relative (e.g., https://your-site.scom/api/...)
 // It relies entirely on your proxy configuration (vercel.json or vite.config.js)
 const baseURL = "https://api.friendcircles.in/api";
 
